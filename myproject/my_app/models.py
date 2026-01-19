f58169c34e618b292376f49c96ba87ac0ede21c1
Eugene <eugenemisiko438@gmail.com>
2026-01-19 23:06:39 +0300

    added the table inthe models.py that Represents a food item consumed by the user.

--- myproject/my_app/models.py
+++ myproject/my_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class FoodItem(models.Model):
+    """
+    Represents a food item consumed by the user.
+    """
+    name = models.CharField(max_length=100)
+    carlories = models.PositiveIntegerField()
+    date_added = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.name} - {self.carlories} kcal"
